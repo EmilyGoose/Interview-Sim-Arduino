86445c9cb03ebfd9c3eabd2e3a6d3784f796f58c
Emily Goose <EmilyGoose@users.noreply.github.com>
2023-01-14 18:26:38 -0500

    Gyro Works

--- jobsim-arduino.ino
+++ jobsim-arduino.ino
@@ -3,28 +3,33 @@
 // Dependency: https://github.com/morrissinger/ESP8266-Websocket
 #include <WebSocketClient.h>
 
+// I like wire
+#include "Wire.h" // This library allows you to communicate with I2C devices.
+
+// Wifi connection vars
 const char* ssid     = "trans rights";
 const char* password = "arduinotime";
 char path[] = "/";
-char host[] = "192.168.137.247";
-int port = 7101;
-  
+char host[] = "192.168.137.1";
+int port = 1989;
+
 WebSocketClient webSocketClient;
 
 // Use WiFiClient class to create TCP connections
 WiFiClient client;
 
+// MPU stuff
+const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
+int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
+int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
+int16_t temperature; // variables for temperature data
+
 void setup() {
+
   Serial.begin(9600);
   delay(10);
 
   // We start by connecting to a WiFi network
-
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  
   WiFi.begin(ssid, password);
   
   while (WiFi.status() != WL_CONNECTED) {
@@ -32,14 +37,8 @@ void setup() {
     Serial.print(".");
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  delay(5000);
+  delay(2500);
   
-
   // Connect to the websocket server
   if (client.connect(host, port)) {
     Serial.println("Connected");
@@ -62,10 +61,34 @@ void setup() {
     }  
   }
 
+  
+  // Begin transmission to the MPU-6050
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
+  Wire.write(0x6B); // PWR_MGMT_1 register
+  Wire.write(0); // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+
 }
 
 
 void loop() {
+
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
+  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
+  Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
+
+  // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
+  accelerometer_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+  accelerometer_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+  accelerometer_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
+  temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
+  gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
+  gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
+  gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+  
+
   String data;
 
   if (client.connected()) {
@@ -79,7 +102,7 @@ void loop() {
     // capture the value of analog 1, send it along
     // pinMode(1, INPUT);
     // data = String(analogRead(1));
-    data = "HELLO WORLD I EXIST";
+    data = "GYRO TIME\n" + String(gyro_x) + "\n" + String(gyro_y) + "\n" + String(gyro_z);
     
     webSocketClient.sendData(data);
     
@@ -91,6 +114,6 @@ void loop() {
   }
   
   // wait to fully let the client disconnect
-  delay(3000);
+  delay(1000);
   
 }
